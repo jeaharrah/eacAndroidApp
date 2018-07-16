47909893a9ec1e737480c600d847cb1afaadd5ec
Jennifer A'Harrah <jeaharrah@gmail.com>
2018-07-16 18:30:33 -0400

    Renamed Login activity to GoogleSignIn to make a separate activity for Google (just temporarily). Created a content layout file for Google Sign-In.

--- app/src/main/java/com/example/jeaha/navigationdrawer/GoogleSignIn.java
+++ app/src/main/java/com/example/jeaha/navigationdrawer/GoogleSignIn.java
@@ -3,14 +3,11 @@ package com.example.jeaha.navigationdrawer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.facebook.FacebookSdk;
-import com.facebook.appevents.AppEventsLogger;
-
-public class LoginPortal extends AppCompatActivity {
+public class GoogleSignIn extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_portal);
+        setContentView(R.layout.activity_google_sign_in_portal);
     }
 }

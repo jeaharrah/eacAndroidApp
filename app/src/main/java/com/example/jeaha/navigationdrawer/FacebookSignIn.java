608d1befe58efd545c3415b2d9fc91ddb066b0b2
Jennifer A'Harrah <jeaharrah@gmail.com>
2018-07-15 21:06:49 -0400

    Files for integrating Facebook Sign-On.

---
+++ app/src/main/java/com/example/jeaha/navigationdrawer/FacebookSignIn.java
@@ -0,0 +1,42 @@
+package com.example.jeaha.navigationdrawer;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.widget.LoginButton;
+
+public class FacebookSignIn extends AppCompatActivity {
+
+    CallbackManager callbackManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_facebook_sign_in);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        LoginButton loginButton = findViewById(R.id.facebook_login_button);
+
+        callbackManager = CallbackManager.Factory.create();
+
+    }
+
+}

30b5287ddba0d6f6d8d9aa8d05019391d62c87b5
Jennifer A'Harrah <jeaharrah@gmail.com>
2018-07-17 15:49:50 -0400

    Modified layout code for Google Sign-In activity and formatted Facebook Sign-In files similarly. *Got Google Sign-In to work!!

--- app/src/main/java/com/example/jeaha/navigationdrawer/GoogleSignIn.java
+++ app/src/main/java/com/example/jeaha/navigationdrawer/GoogleSignIn.java
@@ -2,9 +2,9 @@ package com.example.jeaha.navigationdrawer;
 
 import android.content.Intent;
 import android.support.annotation.NonNull;
-import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -16,18 +16,9 @@ import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
-import com.google.android.gms.common.api.Api;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.PendingResult;
 import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.common.api.Status;
-import com.google.android.gms.signin.SignIn;
-
-import org.w3c.dom.Text;
-
-import java.io.FileDescriptor;
-import java.io.PrintWriter;
-import java.util.concurrent.TimeUnit;
 
 public class GoogleSignIn extends AppCompatActivity implements GoogleApiClient
         .OnConnectionFailedListener, View.OnClickListener {
@@ -43,7 +34,11 @@ public class GoogleSignIn extends AppCompatActivity implements GoogleApiClient
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_google_sign_in_portal);
+        setContentView(R.layout.activity_google_sign_in);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions
                 .DEFAULT_SIGN_IN).requestEmail().build();
@@ -52,9 +47,11 @@ public class GoogleSignIn extends AppCompatActivity implements GoogleApiClient
 
         statusTextView = (TextView) findViewById(R.id.status_txtView);
         signInButton = (SignInButton) findViewById(R.id.sign_in_button);
+        //findViewById(R.id.sign_in_button).setOnClickListener(this);
         signInButton.setOnClickListener(this);
 
         signOutButton = (Button) findViewById(R.id.sign_out_button);
+        //findViewById(R.id.sign_out_button).setOnClickListener(this);
         signOutButton.setOnClickListener(this);
 
     }
@@ -96,7 +93,7 @@ public class GoogleSignIn extends AppCompatActivity implements GoogleApiClient
         if (result.isSuccess()) {
             //Signed in successfully, show authenticated UI.
             GoogleSignInAccount acct = result.getSignInAccount();
-            statusTextView.setText("Hello, " + acct.getDisplayName());
+            statusTextView.setText("Hello, " + acct.getDisplayName() + "!");
         } else {
 
         }

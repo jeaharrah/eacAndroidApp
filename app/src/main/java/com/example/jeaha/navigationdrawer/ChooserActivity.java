735a566ff09851023ae34e5ac87f820e4d1e14aa
Jennifer A'Harrah <jeaharrah@gmail.com>
2018-08-05 17:49:32 -0400

    Added code in the Chooser Activity that sets the title of each item in the list of signin option so that the title is not automatically set as the title of the corresponding Java class.

--- app/src/main/java/com/example/jeaha/navigationdrawer/ChooserActivity.java
+++ app/src/main/java/com/example/jeaha/navigationdrawer/ChooserActivity.java
@@ -2,11 +2,9 @@ package com.example.jeaha.navigationdrawer;
 
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,21 +13,25 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
-import com.elmargomez.typer.Font;
-import com.elmargomez.typer.Typer;
-
 public class ChooserActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
     private static final Class[] CLASSES = new Class[]{
             GoogleSignInActivity.class,
             FacebookLoginActivity.class,
-            EmailPasswordActivity.class,
+            EmailPasswordActivity.class
     };
+
+    private static final String[] CLASS_NAMES = new String[]{
+            "Google Sign In",
+            "Facebook Sign In",
+            "Email & Password Sign In"
+    };
+
 
     private static final int[] DESCRIPTION_IDS = new int[]{
             R.string.desc_google_sign_in,
             R.string.desc_facebook_login,
-            R.string.desc_emailpassword,
+            R.string.desc_emailpassword
 
     };
 
@@ -42,21 +44,13 @@ public class ChooserActivity extends AppCompatActivity implements AdapterView.On
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        TextView tv1 = findViewById(R.id.chooser_header);
-        TextView tv2 = findViewById(R.id.chooser_caption);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            tv2.setJustificationMode(Layout.JUSTIFICATION_MODE_INTER_WORD);
-        } else {
-            tv2.setTextAlignment(View.TEXT_ALIGNMENT_VIEW_START);
-        }
-
         // Set up ListView and Adapter
         ListView listView = findViewById(R.id.list_view);
-        tv1.setTypeface(Typer.set(ChooserActivity.this).getFont(Font.ROBOTO_BLACK));
-        tv2.setTypeface(Typer.set(ChooserActivity.this).getFont(Font.ROBOTO_REGULAR));
 
-        MyArrayAdapter adapter = new MyArrayAdapter(this, android.R.layout.simple_list_item_2, CLASSES);
+        MyArrayAdapter adapter = new MyArrayAdapter(this, android.R.layout.simple_list_item_2,
+                CLASSES, CLASS_NAMES);
         adapter.setDescriptionIds(DESCRIPTION_IDS);
+        adapter.setClassNames(CLASS_NAMES);
 
         listView.setAdapter(adapter);
         listView.setOnItemClickListener(this);
@@ -73,12 +67,14 @@ public class ChooserActivity extends AppCompatActivity implements AdapterView.On
         private Context mContext;
         private Class[] mClasses;
         private int[] mDescriptionIds;
+        private String[] mClassNames;
 
-        public MyArrayAdapter(Context context, int resource, Class[] objects) {
+        public MyArrayAdapter(Context context, int resource, Class[] objects, String[] classNames) {
             super(context, resource, objects);
 
             mContext = context;
             mClasses = objects;
+            mClassNames = classNames;
         }
 
         @Override
@@ -90,7 +86,10 @@ public class ChooserActivity extends AppCompatActivity implements AdapterView.On
                 view = inflater.inflate(android.R.layout.simple_list_item_2, null);
             }
 
-            ((TextView) view.findViewById(android.R.id.text1)).setText(mClasses[position].getSimpleName());
+            TextView tv1 = view.findViewById(android.R.id.text1);
+            tv1.setPadding(0, 3, 0, 3);
+
+            ((TextView) view.findViewById(android.R.id.text1)).setText(mClassNames[position]);
             ((TextView) view.findViewById(android.R.id.text2)).setText(mDescriptionIds[position]);
 
             return view;
@@ -99,5 +98,10 @@ public class ChooserActivity extends AppCompatActivity implements AdapterView.On
         public void setDescriptionIds(int[] descriptionIds) {
             mDescriptionIds = descriptionIds;
         }
+
+        public void setClassNames(String[] classNames) {
+            mClassNames = classNames;
+        }
+
     }
 }

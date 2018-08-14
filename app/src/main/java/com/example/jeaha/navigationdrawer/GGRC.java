e1855fa5aa52558679c40f0850c01cd3994c9623
Jennifer A'Harrah <jeaharrah@gmail.com>
2018-08-14 19:39:41 -0400

    Added logic to the GGRC activity for trying to retrieve the signed-in user's name or email and including this information in a personalized greeting message. If the user is null, I want it to display that the user needs to sign in. So far, this code is not working and I will need to take a different course of action.

--- app/src/main/java/com/example/jeaha/navigationdrawer/GGRC.java
+++ app/src/main/java/com/example/jeaha/navigationdrawer/GGRC.java
@@ -5,13 +5,23 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseUser;
 
 
 public class GGRC extends AppCompatActivity {
 
-    private FacebookLoginActivity userName;
+    private FacebookLoginActivity fbInstance = new FacebookLoginActivity();
+    private GoogleSignInActivity googleInstance = new GoogleSignInActivity();
+    private EmailPasswordActivity emailInstance = new EmailPasswordActivity();
+    private String name;
+
+    private String fbName;
+    private String googleEmail;
+    private String email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,8 +41,55 @@ public class GGRC extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         TextView userTxtView = findViewById(R.id.ggrc_user_txtView);
-        String name = userName.user.getDisplayName();
-        userTxtView.setText(getString(R.string.ggrc_user_greeting_fmt, name));
+
+        /*try {
+            fbName = fbInstance.user.getDisplayName();
+        } catch (NullPointerException e) {
+            Log.e("GGRC", "Facebook User NullPointerException: ", e);
+        }
+
+        if (fbName == null) {
+            try {
+                googleEmail = googleInstance.user.getEmail();
+            } catch (NullPointerException e) {
+                Log.e("GGRC", "Google User NullPointerException: ", e);
+            }
+        }
+        try {
+            email = emailInstance.user.getEmail();
+        } catch (NullPointerException e) {
+            Log.e("GGRC", "Email User NullPointerException: ", e);
+        }
+
+
+        if (fbName == null & googleEmail == null & email == null) {
+            userTxtView.setText("Please sign in to track your GoGreen Rewards Card credits!");
+        } else {
+            if (fbName != null) {
+                name = fbName;
+            } else if (googleEmail != null) {
+                name = googleEmail;
+            } else {
+                name = email;
+            }
+        }*/
+        String message = "";
+        String authName = "";
+
+        /*try {
+            authName = googleInstance.mAuth
+                    .getCurrentUser().getDisplayName();
+        } catch (NullPointerException e) {
+            Log.e("GGRC", "NullPointerException", e);
+        }
+
+        if (authName == null) {
+            message = "Please log in.";
+        } else {
+            message = getString(R.string.ggrc_user_greeting_fmt, authName);
+        }
+
+        userTxtView.setText(message);*/
 
     }
 

6e29193f29b9d581767c6c8a0eaf01814458c6d8
Jennifer A'Harrah <jeaharrah@gmail.com>
2018-08-17 18:56:41 -0400

    Removed unnecessary scrollbar logic in GGRC.java.

--- app/src/main/java/com/example/jeaha/navigationdrawer/GGRC.java
+++ app/src/main/java/com/example/jeaha/navigationdrawer/GGRC.java
@@ -5,14 +5,10 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.ToggleButton;
-
-import com.google.firebase.auth.FirebaseUser;
-
 
 public class GGRC extends AppCompatActivity {
 
@@ -115,7 +111,6 @@ public class GGRC extends AppCompatActivity {
 
     public void onToggleClicked(View view) {
         greenActionsLayout = findViewById(R.id.greenActionsList);
-        //greenActionsLayout.setScrollContainer(true);
 
         if (((ToggleButton) view).isChecked()) {
             greenActionsLayout.setVisibility(View.VISIBLE);

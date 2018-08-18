68f197f5e729b9ee1a2b4c72ae591112739da82d
Jennifer A'Harrah <jeaharrah@gmail.com>
2018-08-17 20:58:21 -0400

    Commented out the floating action button retrieval code in the GGRC Java class. (I commented out the XML code for the FAB widget in this activity because I currently do not have a use for it. Obviously, the application will stop at runtime if I try to call a nonexistent object in the corresponding Java class. Haha.)

--- app/src/main/java/com/example/jeaha/navigationdrawer/GGRC.java
+++ app/src/main/java/com/example/jeaha/navigationdrawer/GGRC.java
@@ -30,14 +30,14 @@ public class GGRC extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+/*        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         TextView userTxtView = findViewById(R.id.ggrc_user_txtView);
